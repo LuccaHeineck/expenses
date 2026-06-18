83ca6d8a1c65a9921d9d523dcff25b3276c29542
LuccaHeineck <luccacheineck@gmail.com>
2026-06-18 19:43:18 -0300

    test final

---
+++ migrations/0011_breezy_red_wolf.sql
@@ -0,0 +1,4 @@
+CREATE TABLE "categoria" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"nome" varchar(120) NOT NULL
+);
--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,8 @@ import { createPageRouter } from './routes/pageRoutes';
 
 dotenv.config();
 
+const inutil = 1;
+
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 app.disable('x-powered-by');
